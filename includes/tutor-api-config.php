a7fcdbb79a14e9f0109f4f6457742493542c6c1a
mahmoud <mzughbor@gmail.com>
2025-02-24 23:35:58 +0200

    fix categories

--- includes/tutor-api-config.php
+++ includes/tutor-api-config.php
@@ -7,7 +7,7 @@ class TutorAPIConfig {
     
     // Update with the provided credentials
     const API_KEY = 'key_1a3b36b770d1597540eeafd73dc36ba1';
-    const API_SECRET = '****';
+    const API_SECRET = 'secret_54ec6ad12a9056f8b2281a071acc38009d38e25a1c904a12eeec11395f0c6f99';
     
     // Endpoints
     const ENDPOINT_COURSES = '/tutor/v1/courses';
