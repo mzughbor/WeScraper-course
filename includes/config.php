72bd9f7a57b90528aa1ccb3036b51ae483245265
mahmoud <mzughbor@gmail.com>
2025-02-27 02:32:15 +0200

    finish adding uploading thumbnail

--- includes/config.php
+++ includes/config.php
@@ -5,6 +5,6 @@
 $site_url = 'https://wedti.com';
 $api_url = $site_url . '/wp-json/wp/v2/course-category';
 $username = 'admin';
-$password = 'mKw1 aK7x ORm6 ogzO 20OE TtfN';
+$password = '';
 
 ?>
--- includes/tutor-api-config.php
+++ includes/tutor-api-config.php
@@ -6,8 +6,8 @@ class TutorAPIConfig {
     const API_BASE_URL = 'https://wedti.com/wp-json';
     
     // Update with the provided credentials
-    const API_KEY = 'key_b51d74eb8c1825d630090a858fcd681f';
-    const API_SECRET = 'secret_7bce73fdbf7d7fc3f7ea04194d8fa7df0cb7ed2e3806fc2bc26311532cd42dac';
+    const API_KEY = '';
+    const API_SECRET = '';
     
     // Endpoints
     const ENDPOINT_COURSES = '/tutor/v1/courses';
